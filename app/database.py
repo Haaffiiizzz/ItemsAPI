4c56dbded67399bbb7916b22e784396b5c9548d1
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2024-07-09 22:43:20 -0600

    Tidying up

--- app/database.py
+++ app/database.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from .config import settings
 import os
 
 # here we just kinda start the database 
--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from sqlalchemy import Table, MetaData
+from sqlalchemy import MetaData
 from .models import Base
 from .routers import users, country, auth
 from .database import engine
