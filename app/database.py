e96c183d90b893b8b6a051fb02be20aaf35281a7
Abdulhafiz Oladimeji Dada <158800417+Haaffiiizzz@users.noreply.github.com>
2024-07-12 18:45:55 -0600

    Took out comments

--- app/database.py
+++ app/database.py
@@ -3,17 +3,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-# here we just kinda start the database 
+
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Use the environment variable directly
+
 engine = create_engine(DATABASE_URL, connect_args={"sslmode": "require"})
-
-# SQL_DB_URL = f"postgresql://{settings.DATABASE_USERNAME}:{settings.DATABASE_PASSWORD}@{settings.DATABASE_HOSTNAME}:{settings.DATABASE_PORT}/{settings.DATABASE_NAME}?sslmode=require"
-# # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
-
-# engine = create_engine(SQL_DB_URL)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) 
 
 Base = declarative_base()
