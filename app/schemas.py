56a0a0d16d82025ed0601d29e4cafa726925757a
Abdulhafiz Oladimeji Dada <158800417+Haaffiiizzz@users.noreply.github.com>
2024-07-12 18:46:38 -0600

    Took out comments

--- app/schemas.py
+++ app/schemas.py
@@ -2,10 +2,7 @@ from pydantic import BaseModel, EmailStr
 from typing import Optional
 from datetime import datetime
 
-# this is where the models for the input and outputs are
 class AddData(BaseModel):
-    #  this makes sure we are getting the right data format else it
-    # throws an error
     items: dict
 
 class CreateUser(BaseModel):
