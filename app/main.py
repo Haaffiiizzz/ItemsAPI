621943fd6875014f5ffa483d6e3b3e1a8c538f8d
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2025-01-26 04:14:15 -0600

    .

--- app/main.py
+++ app/main.py
@@ -1,32 +1,24 @@
-# from fastapi import FastAPI
-# from fastapi.middleware.cors import CORSMiddleware
-# from sqlalchemy import MetaData
-# from .models import Base
-# from .routers import users, country, auth
-# from .database import engine
-
-
-# metadata = MetaData()
-
-# Base.metadata.create_all(bind=engine)
-# app = FastAPI()
-
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
-# app.include_router(country.router)
-# # app.include_router(users.router)
-# # app.include_router(auth.router)    # add the routers from the other files
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy import MetaData
+from .models import Base
+from .routers import users, country, auth
+from .database import engine
 
+
+metadata = MetaData()
+
+Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, Vercel!"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(country.router)
+# app.include_router(users.router)
+# app.include_router(auth.router)    # add the routers from the other files
