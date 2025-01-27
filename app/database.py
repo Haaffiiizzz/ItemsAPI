9c859a97362da249ab839b36f968ab6ce1907326
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2025-01-26 23:25:43 -0600

    database url

--- app/database.py
+++ app/database.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from .config import settings
+import os
 
-engine = create_engine(settings.DATABASE_URL, connect_args={"sslmode": "require"})
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL, connect_args={"sslmode": "require"})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
